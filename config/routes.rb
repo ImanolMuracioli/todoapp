da6666b5f2b48f9e07c1edf7d8eb922a6076a416
ImanolMuracioli <imamuracioli@gmail.com>
2021-11-10 19:29:27 -0300

    Version API

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :items
+  namespace :api do 
+    namespace :v1 do
+      resources :items
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

64b0d04fd30cba0d954417e3f047e26fab782313
ImanolMuracioli <imamuracioli@gmail.com>
2021-11-10 20:04:10 -0300

    Define relation folder-item

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_222005) do
+ActiveRecord::Schema.define(version: 2021_11_10_225944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "folders", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.text "description"
     t.boolean "mark"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "folder_id"
   end
 
 end

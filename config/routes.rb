89e0c727aa909f36d09c35119c1ce39cfbb5d874
ImanolMuracioli <imamuracioli@gmail.com>
2021-11-10 19:56:57 -0300

    Adding model Folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :folders
   namespace :api do 
     namespace :v1 do
       resources :items
+      resources :folders
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
---
+++ db/migrate/20211110225528_create_folders.rb
@@ -0,0 +1,9 @@
+class CreateFolders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :folders do |t|
+      t.text :name
+
+      t.timestamps
+    end
+  end
+end
